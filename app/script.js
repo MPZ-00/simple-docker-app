5638d52021a88cc0d8c2ce8f1068aab074b9a11d
Martin <martin.praetzlich@outlook.de>
2023-05-27 15:48:58 +0200

    server script added

--- app/script.js
+++ app/script.js
@@ -52,13 +52,16 @@ function updateProfile() {
     contEdit.style.display = 'block'
 }
 
+var themes = ["dark", "light", "vintage", "summer"];
+var currentTheme = 0;
+
 function toggleTheme() {
-    var htmlClassList = document.documentElement.classList;
-    if (htmlClassList.contains("dark")) {
-        htmlClassList.remove("dark");
-        document.documentElement.setAttribute("data-theme", "light");
-    } else {
-        htmlClassList.add("dark");
-        document.documentElement.setAttribute("data-theme", "dark");
-    }
+    currentTheme = (currentTheme + 1) % themes.length;
+
+    var rootStyles = document.documentElement.style;
+    rootStyles.setProperty("--text-color", "var(--text-color-" + themes[currentTheme] + ")");
+    rootStyles.setProperty("--background-color", "var(--background-color-" + themes[currentTheme] + ")");
+    rootStyles.setProperty("--button-background-color", "var(--button-background-color-" + themes[currentTheme] + ")");
+    rootStyles.setProperty("--button-text-color", "var(--button-text-color-" + themes[currentTheme] + ")");
+    rootStyles.setProperty("--button-hover-background-color", "var(--button-hover-background-color-" + themes[currentTheme] + ")");
 }
